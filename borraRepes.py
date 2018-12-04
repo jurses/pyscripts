de9e33f6c7894f8c716fc48f3aad406869dccf43
Raúl Ulises Martín Hernández <martin.jurses@gmail.com>
2018-12-04 20:38:30 +0000

    pequeña modificación crucial

--- borraRepes.py
+++ borraRepes.py
@@ -19,26 +19,34 @@ def openFile():
     fileName = filedialog.askopenfilename(initialdir = ".", title = "Select file", filetypes = (("Archivos Excel","*.xlsx"), ("Cualquier archivo","*.*")))
 
 def removeWholeRow(cell, ws):
-    for cellRows in ws.iter_rows(min_col = column_index_from_string("A"), max_col = 70, min_row = cell.row, max_row = cell.row):
+    for cellRows in ws.iter_rows(min_col = column_index_from_string("A"), max_col = 35, min_row = cell.row, max_row = cell.row):
         for x in cellRows:
             x.value = None
 
+def concatenateColElements(vcell):
+    for v in vcell:
+        concatS += str(v.value)
+        concatS += ","
+
+    return concats 
+
 def removeUntilNextChange():
     wb = load_workbook(filename=fileName, data_only=True)
-    ws = wb[e1.get()]
+    ws = wb[str(e1.get())]
     col = column_index_from_string(e2.get())
     row_init = int(e3.get())
     row_end = int(e4.get())
 
     newValue = None
     for cellRows in ws.iter_rows(min_col = col, max_col = col, min_row = row_init, max_row = row_end):
-        if newValue != cellRows[0].value:
-            newValue = cellRows[0].value
-        else:
+        if newValue == cellRows[0].value:
             if allRow.get():
-                removeWholeRow(cellRows[0], ws)
+                removeWholeRow(cellRows[0], ws) # para toda esa fila
+            else:
+                cellRows[0].value = None    # para solamente esa columna
+        else:
+            newValue = cellRows[0].value
 
-            cellRows[0].value = None
 
     wb.save("{}_modBorraRepes.xlsx".format(fileName))
         
@@ -55,11 +63,12 @@ Label(root, text="Hoja del libro").grid(row = 0)
 Label(root, text="Columna").grid(row = 1)
 Label(root, text="Fila de inicio").grid(row = 2)
 Label(root, text="Fila final").grid(row = 3)
+Label(root, text = "Columnas referentes").grid(row = 4)
 #Label(root, text="Nombre del archivo: {}".format(fileName)).grid(row = 5)
 Button(root, text="Aceptar", command=removeUntilNextChange).grid(row = 4, column = 1, sticky=W, pady=4)
-Button(root, text="Salir", command=root.quit).grid(row = 4, column = 2, sticky=W, pady=4)
-Button(root, text = "Abrir archivo", command = openFile).grid(row = 4, column = 0, sticky = W, pady = 4)
-Checkbutton(root, text = "Aplicar eliminación a toda la fila", variable = allRow, onvalue = TRUE, offvalue = FALSE).grid(row = 3, column = 2, sticky = W, pady =4)
+Button(root, text="Salir", command=root.quit).grid(row = 5, column = 2, sticky=W, pady=4)
+Button(root, text = "Abrir archivo", command = openFile).grid(row = 5, column = 0, sticky = W, pady = 4)
+Checkbutton(root, text = "Aplicar eliminación a toda la fila", variable = allRow, onvalue = TRUE, offvalue = FALSE).grid(row = 4, column = 2, sticky = W, pady =4)
 
 e1 = Entry(root)
 e2 = Entry(root)
