3f7275645aec549da01152db2c47e95eb4c9230c
Raúl Ulises Martín Hernández <martin.jurses@gmail.com>
2018-12-06 12:21:50 +0000

    Por terminar 1/2

--- borraRepes.py
+++ borraRepes.py
@@ -13,71 +13,110 @@ from tkinter import *
 from tkinter import filedialog
 from openpyxl import load_workbook
 from openpyxl.utils import *
+import re
+
+global allRow
 
 def openFile():
     global fileName
     fileName = filedialog.askopenfilename(initialdir = ".", title = "Select file", filetypes = (("Archivos Excel","*.xlsx"), ("Cualquier archivo","*.*")))
+    print(fileName)
 
-def removeWholeRow(cell, ws):
-    for cellRows in ws.iter_rows(min_col = column_index_from_string("A"), max_col = 35, min_row = cell.row, max_row = cell.row):
-        for x in cellRows:
-            x.value = None
+def cellIsInVCells(cell, vCells):
+    for v in vCells:
+        if cell.value == v.value:
+            return True
+    
+    return False
 
-def concatenateColElements(vcell):
-    for v in vcell:
-        concatS += str(v.value)
-        concatS += ","
+def getVCells(row, refCols, min_col = 0):
+    vCells = []
+    for col in refCols:
+        vCells.append(row[col - min_col])
+    
+    return vCells
 
-    return concats 
+#   Obtiene las columnas de referencias pasadas por una cadena
+#   Para ser usadas con openpyxl
+def getVRefCols(columns_string):
+    columns_string = columns_string.replace(" ", "")
+    vCols = []
+    if len(columns_string) > 1:
+        for v in columns_string.split(','):
+            vCols.append(column_index_from_string(v))
+    else:
+        vCols.append(column_index_from_string(columns_string))
+    
+    return vCols
+
+def getCornerMatrix(matrix):
+    matrixSelected = []
+
+    matrix = matrix.replace(" ", "")
+    matrix = matrix.split(':')
+    
+    matrixSelected.append("".join(re.split("[^a-zA-Z]*", matrix[0])))
+    matrixSelected.append("".join(re.split("[^0-9]*", matrix[0])))
+    matrixSelected.append("".join(re.split("[^a-zA-Z]*", matrix[1])))
+    matrixSelected.append("".join(re.split("[^0-9]*", matrix[1])))
+
+    return matrixSelected
 
 def removeUntilNextChange():
+    assert(fileName)
     wb = load_workbook(filename=fileName, data_only=True)
-    ws = wb[str(e1.get())]
-    col = column_index_from_string(e2.get())
-    row_init = int(e3.get())
-    row_end = int(e4.get())
+    ws = wb[e1.get()]
+    print(e1.get())
+    matrixSelected = getCornerMatrix(e2.get())
+    vRefCols = getVRefCols(e5.get())
+    print(e5.get())
 
-    newValue = None
-    for cellRows in ws.iter_rows(min_col = col, max_col = col, min_row = row_init, max_row = row_end):
-        if newValue == cellRows[0].value:
+    currentValue = None
+
+    for col in vRefCols:
+        vCell = getVCells(cellRows, vRefCols, column_index_from_string(col))
+        if cellIsInVCells(cellRows[col], vCell):
             if allRow.get():
-                removeWholeRow(cellRows[0], ws) # para toda esa fila
-            else:
-                cellRows[0].value = None    # para solamente esa columna
-        else:
-            newValue = cellRows[0].value
 
+
+    for cellRows in ws.iter_rows(   min_col = column_index_from_string(matrixSelected[0]),
+                                    max_col = column_index_from_string(matrixSelected[2]), 
+                                    min_row = int(matrixSelected[1]),
+                                    max_row = int(matrixSelected[3])
+                                ):
+        for col in vRefCols:
+            vCell = getVCells(cellRows, vRefCols, column_index_from_string(col))
+            if cellIsInVCells(cellRows[col], vCell):
+                if allRow.get():
+                else:
+                    cellRows[col] = None
 
     wb.save("{}_modBorraRepes.xlsx".format(fileName))
         
 root = Tk()
 root.title("Herramientas Excel")
-global allRow
-allRow = BooleanVar()
-fileName = None
 
 root.geometry("500x500")
 root.resizable(0, 0)
 
-Label(root, text="Hoja del libro").grid(row = 0)
-Label(root, text="Columna").grid(row = 1)
-Label(root, text="Fila de inicio").grid(row = 2)
-Label(root, text="Fila final").grid(row = 3)
-Label(root, text = "Columnas referentes").grid(row = 4)
-#Label(root, text="Nombre del archivo: {}".format(fileName)).grid(row = 5)
-Button(root, text="Aceptar", command=removeUntilNextChange).grid(row = 4, column = 1, sticky=W, pady=4)
-Button(root, text="Salir", command=root.quit).grid(row = 5, column = 2, sticky=W, pady=4)
-Button(root, text = "Abrir archivo", command = openFile).grid(row = 5, column = 0, sticky = W, pady = 4)
-Checkbutton(root, text = "Aplicar eliminación a toda la fila", variable = allRow, onvalue = TRUE, offvalue = FALSE).grid(row = 4, column = 2, sticky = W, pady =4)
+allRow = BooleanVar()
+
+
+l1 = Label(root, text="Hoja del libro").grid(row = 0)
+l2 = Label(root, text = "Seleccione la matriz\nEjemplo: D3:F5").grid(row = 1)
+l5 = Label(root, text = "Columnas referentes\nSeparadas por ','").grid(row = 2)
+Button(root, text="Aceptar", command=removeUntilNextChange).grid(row = 3, column = 1, sticky=W, pady=4)
+Button(root, text="Salir", command=root.quit).grid(row = 3, column = 2, sticky=W, pady=4)
+Button(root, text = "Abrir archivo", command = openFile).grid(row = 3, column = 0, sticky = W, pady = 4)
+Checkbutton(root, text = "Aplicar eliminación a toda la fila", variable = allRow, onvalue = TRUE, offvalue = FALSE).grid(row = 1, column = 2, sticky = W, pady = 4)
 
 e1 = Entry(root)
 e2 = Entry(root)
-e3 = Entry(root)
-e4 = Entry(root)
+e5 = Entry(root)
 
 e1.grid(row = 0, column = 1)
 e2.grid(row = 1, column = 1)
-e3.grid(row = 2, column = 1)
-e4.grid(row = 3, column = 1)
+e5.grid(row = 2, column = 1)
+
 
 mainloop()
